ef5c7eeb1980209018852b601ce2fdd7a2fef657
Daniel Brandan <dhanbrandan@gmail.com>
2023-09-15 17:51:29 -0300

    Se añadio 2 funciones mas

--- funciones.js
+++ funciones.js
@@ -3,3 +3,11 @@ console.log ("HOLA MUNDITO");
 export const function (params) {
     return true;
 }
+
+export const function (params) {
+    return true;
+}
+
+export const function (params) {
+    return true;
+}
